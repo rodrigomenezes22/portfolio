8f29fb45b0c5711187d1687de5a6b859b9b9a3ff
rodrigomenezes22 <69732792+rodrigomenezes22@users.noreply.github.com>
2023-02-26 17:05:20 +0100

    Logo and css fixes.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -49,7 +49,7 @@ export default function Header({linkedin, github , scrollToTop}) {
             <div className='container'>
       <nav className="navbar"  role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-            <a className="navbar-item" href="#hero">
+            <a className="navbar-item bebas logo" href="#hero">
                 Rodrigo M.
             </a>
 
